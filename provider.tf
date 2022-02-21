4c1533ae603b6ae7df63c5c09de658c6e37cc6ac
Karthick Dharman <karthick.dharman@bootlabstech.com>
2022-02-21 19:15:51 +0530

    <No ID> <build fix>

--- provider.tf
+++ provider.tf
@@ -1,10 +1,10 @@
 ### Hashicorp Terraform Provider Configuration File ###
 terraform {
   required_version = ">=0.13"
-  
+
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "4.1.0"
     }
   }
