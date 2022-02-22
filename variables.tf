ad8aaf0e4c5fd4945e9af30fed16238fadeede63
Silpashree1 <94901964+Silpashree1@users.noreply.github.com>
2022-02-22 21:38:41 +0530

    updated api datatypes.

--- variables.tf
+++ variables.tf
@@ -22,43 +22,14 @@ variable "force_destroy" {
 
 variable "location" {
   description = <<-EOT
-    {
-   "type": "json",
+  {
+   "type": "api",
    "purpose": "autocomplete",
-   "data":[ "asia-east1",
-        "asia-east2",
-        "asia-northeast1",
-        "asia-northeast2",
-        "asia-northeast3",
-        "asia-south1",
-        "asia-south2",
-        "asia-southeast1",
-        "asia-southeast2",
-        "australia-southeast1",
-        "australia-southeast2",
-        "europe-central2",
-        "europe-north1",
-        "europe-west1",
-        "europe-west2",
-        "europe-west3",
-        "europe-west4",
-        "europe-west6",
-        "northamerica-northeast1",
-        "northamerica-northeast2",
-        "southamerica-east1",
-        "southamerica-west1",
-        "us-central1",
-        "us-east1",
-        "us-east4",
-        "us-west1",
-        "us-west2",
-        "us-west3",
-        "us-west4"
-    ],
-   "description": "the location of the bucket"
+   "data": "/api/v1/autocomplete/regions",
+   "description": "regions for the GCS"
 }
-  EOT
-  type        = string
+EOT
+type        = string
 }
 
 variable "project_id" {
